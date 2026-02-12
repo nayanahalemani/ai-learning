2f3c96ca5b81939e5168183468d67d4ce0d82fea
Nayana <nayana.halemani@gmail.com>
2026-02-11 20:10:50 -0800

    first python script

---
+++ hello_ai.py
@@ -0,0 +1,13 @@
+name=input('Please enter your name: ')
+print('Hello')
+print (name)
+
+##print statement for easy debug
+print('I am doing some Math here..')
+x=530+12
+print('x= ',x)
+print('y=x/0 ')
+y=x/0
+print('y= ',y)
+print('Dividing x by zero')
+       
