7b48760750533d014388cc406e04b0bc7a550dba
Nayana <nayana.halemani@gmail.com>
2026-02-11 21:05:32 -0800

    intro file

---
+++ introProfile.py
@@ -0,0 +1,8 @@
+name = input('Enter your name: ')
+role= input('Enter your current role: ')
+goal=input('What is your goal for learning Python ')
+
+print('\n-----------PROFILE------------')
+print('Name:',name)
+print('Current Role:',role)
+print('Python Goal:',goal)
